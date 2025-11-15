1f867ea197c2013d26cc93d6d770c024366e7020
Axel VandenHeuvel <axelvandenhe@gmail.com>
2025-11-15 15:29:45 -0700

    attempt at fixing projects page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        project: resolve(__dirname, 'project.html'),
+      },
+    },
+  },
 })
